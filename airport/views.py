be1c87cbcf9e2f91d275490dbc2a4aae040bf6de
Lukian <lukian55d@gmail.com>
2026-09-22 17:36:53 +0100

    Add viewsets, filtering, and url routing for all endpoints

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,119 @@
-from django.shortcuts import render
+from django.db.models import Count, F
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from airport.models import (
+    Airport, Route, AirplaneType, Airplane, Crew, Flight, Order, Ticket
+)
+from airport.serializers import (
+    AirportSerializer,
+    RouteSerializer, RouteListSerializer, RouteDetailSerializer,
+    AirplaneTypeSerializer,
+    AirplaneSerializer, AirplaneListSerializer,
+    CrewSerializer,
+    FlightSerializer, FlightListSerializer, FlightDetailSerializer,
+    OrderSerializer, OrderListSerializer,
+)
+
+
+class AirportViewSet(viewsets.ModelViewSet):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.select_related("source", "destination")
+    serializer_class = RouteSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return RouteListSerializer
+        if self.action == "retrieve":
+            return RouteDetailSerializer
+        return RouteSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        source = self.request.query_params.get("source")
+        destination = self.request.query_params.get("destination")
+        if source:
+            queryset = queryset.filter(source__name__icontains=source)
+        if destination:
+            queryset = queryset.filter(destination__name__icontains=destination)
+        return queryset
+
+
+class AirplaneTypeViewSet(viewsets.ModelViewSet):
+    queryset = AirplaneType.objects.all()
+    serializer_class = AirplaneTypeSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+
+class AirplaneViewSet(viewsets.ModelViewSet):
+    queryset = Airplane.objects.select_related("airplane_type")
+    serializer_class = AirplaneSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return AirplaneListSerializer
+        return AirplaneSerializer
+
+
+class CrewViewSet(viewsets.ModelViewSet):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+
+class FlightViewSet(viewsets.ModelViewSet):
+    queryset = (
+        Flight.objects.select_related("route__source", "route__destination", "airplane")
+        .prefetch_related("crew", "tickets")
+        .annotate(
+            tickets_available=F("airplane__rows") * F("airplane__seats_in_row")
+            - Count("tickets")
+        )
+    )
+    serializer_class = FlightSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return FlightListSerializer
+        if self.action == "retrieve":
+            return FlightDetailSerializer
+        return FlightSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        source = self.request.query_params.get("source")
+        destination = self.request.query_params.get("destination")
+        date = self.request.query_params.get("date")
+        if source:
+            queryset = queryset.filter(route__source__name__icontains=source)
+        if destination:
+            queryset = queryset.filter(route__destination__name__icontains=destination)
+        if date:
+            queryset = queryset.filter(departure_time__date=date)
+        return queryset
+
+
+class OrderViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, GenericViewSet):
+    queryset = Order.objects.prefetch_related("tickets__flight")
+    serializer_class = OrderSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return OrderListSerializer
+        return OrderSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
